69bdd06d101581255ace86df8a23b3296d9daacd
martmists <martmists@gmail.com>
2018-01-19 23:30:21 +0100

    Add file to pylint output
    Signed-off-by: martmists <martmists@gmail.com>

--- snekchek/format.py
+++ snekchek/format.py
@@ -10,5 +10,10 @@ def vulture_format(data):
 
 
 def pylint_format(data):
+    last_path = ""
     for row in data:
+        if row['path'] != last_path:
+            print(f"File: {row['path']}")
+            last_path = row['path']
+
         print(f"{row['message-id'][0]}:{row['line']:>3}, {row['column']:>2}: {row['message']} ({row['symbol']})")
