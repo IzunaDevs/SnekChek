af23172c4dd171360300bb1cd201778f8a4a0c8a
martmists <martmists@gmail.com>
2018-01-21 09:39:12 +0100

    remove old option
    Signed-off-by: martmists <martmists@gmail.com>

--- snekchek/lint.py
+++ snekchek/lint.py
@@ -43,8 +43,7 @@ class Flake8(Linter):
         file = io.StringIO()
         with contextlib.redirect_stdout(file):
             try:
-                sett = [f"--config={self.confpath}",
-                        f"--mypy-config={self.confpath}"]
+                sett = [f"--config={self.confpath}"]
                 sett.extend(files)
                 flake8.main.cli.main(sett)
             except SystemExit:
