7d1a192925b09bab107672288cc7f29286c37286
martmists <martmists@gmail.com>
2018-01-30 16:19:02 +0100

    Forgot to git add
    Signed-off-by: martmists <martmists@gmail.com>

---
+++ snekchek/utils.py
@@ -0,0 +1,19 @@
+# Stdlib
+from contextlib import contextmanager
+import sys
+
+
+@contextmanager
+def redirect_stdout(new):
+    old = sys.stdout
+    sys.stdout = new
+    yield
+    sys.stdout = old
+
+
+@contextmanager
+def redirect_stderr(new):
+    old = sys.stderr
+    sys.stderr = new
+    yield
+    sys.stderr = old
