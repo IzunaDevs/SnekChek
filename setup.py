72566d3b0826789a10a312eaf11eab02af525046
martmists <martmists@gmail.com>
2018-01-30 14:56:40 +0100

    add extras_require
    Signed-off-by: martmists <martmists@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 # External Libraries
 from setuptools import setup, find_packages
 from snekchek import misc
+from snekchek.lint import get_linters
+from snekchek.secure import get_security
+from snekchek.style import get_stylers
+from snekchek.tool import get_tools
 
 with open("README.rst") as file:
     README = file.read()
 
 with open("requirements.txt") as file:
     REQUIREMENTS = file.readlines()
+
+EXTRAS = {}
+
+for group in [get_linters(), get_security(), get_stylers(), get_tools()]:
+    for linter in group:
+        EXTRAS[linter.__name__.lower()] = linter.requires_install
 
 if __name__ == '__main__':
     setup(
@@ -23,14 +33,18 @@ if __name__ == '__main__':
         url="https://github.com/IzunaDevs/SnekChek",
         packages=find_packages(),
         install_requires=REQUIREMENTS,
+        extras_require=EXTRAS,
         entry_points={
             "console_scripts": ["snekchek = snekchek.__main__:main"]
         },
         keywords=[
-            "lint", "python", "pylint", "flake8", "isort", "snekchek", "snekrc"
+            "lint", "python", "pylint", "flake8", "isort", "vulture",
+            "bandit", "dodgy", "safety", "pytest", "pyroma", "pypi",
+            "yapf", "snekchek", "snekrc"
         ],
         classifiers=[
-            "Development Status :: 4 - Beta", "Environment :: Console",
+            "Development Status :: 5 - Stable",
+            "Environment :: Console",
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
--- snekchek/misc.py
+++ snekchek/misc.py
@@ -1,4 +1,4 @@
 """Misc information for setup.py"""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 description = "Lint wrapper for python modules"  # pylint: disable=invalid-name
