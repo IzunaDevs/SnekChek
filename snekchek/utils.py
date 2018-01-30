5d358862ed1b1410e6d821a01681f4154c33faea
martmists <martmists@gmail.com>
2018-01-30 17:21:56 +0100

    Change redirect_std* to their 3.6 code
    Signed-off-by: martmists <martmists@gmail.com>

--- snekchek/utils.py
+++ snekchek/utils.py
@@ -3,21 +3,41 @@ This module provides redirect_* functions for Python <3.5
 """
 
 # Stdlib
-from contextlib import contextmanager
 import sys
 
 
-@contextmanager
-def redirect_stdout(new):
-    old = sys.stdout
-    sys.stdout = new
-    yield
-    sys.stdout = old
+class _RedirectStream:
+    _stream = None
+
+    def __init__(self, new_target):
+        self._new_target = new_target
+        # We use a list of old targets to make this CM re-entrant
+        self._old_targets = []
+
+    def __enter__(self):
+        self._old_targets.append(getattr(sys, self._stream))
+        setattr(sys, self._stream, self._new_target)
+        return self._new_target
+
+    def __exit__(self, exctype, excinst, exctb):
+        setattr(sys, self._stream, self._old_targets.pop())
 
 
-@contextmanager
-def redirect_stderr(new):
-    old = sys.stderr
-    sys.stderr = new
-    yield
-    sys.stderr = old
+class redirect_stdout(_RedirectStream):
+    """Context manager for temporarily redirecting stdout to another file.
+        # How to send help() to stderr
+        with redirect_stdout(sys.stderr):
+            help(dir)
+        # How to write help() to a file
+        with open('help.txt', 'w') as f:
+            with redirect_stdout(f):
+                help(pow)
+    """
+
+    _stream = "stdout"
+
+
+class redirect_stderr(_RedirectStream):
+    """Context manager for temporarily redirecting stderr to another file."""
+
+    _stream = "stderr"
