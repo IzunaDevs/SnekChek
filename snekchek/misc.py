e0a967173fde3ab74024bb790c30af363da10029
Martmists <martmists@gmail.com>
2019-10-31 16:44:45 +0100

    Shorten line

--- snekchek/misc.py
+++ snekchek/misc.py
@@ -1,4 +1,5 @@
 u"""Misc information for setup.py"""
 
 __version__ = u"0.4.1"
-description = u"Linter/formatter/test-tool wrapper for python modules"  # pylint: disable=invalid-name
+description = (u"Linter/formatter/test-tool "  # pylint: disable=invalid-name
+               u"wrapper for python modules")
