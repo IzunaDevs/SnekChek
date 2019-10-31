98ad492e69ca3eed7e5240cac669845501d7853f
Martmists <martmists@gmail.com>
2019-10-31 16:52:36 +0100

    Version bump for globs

--- snekchek/misc.py
+++ snekchek/misc.py
@@ -1,6 +1,6 @@
 u"""Misc information for setup.py"""
 
-__version__ = u"0.4.2"
+__version__ = u"0.4.3"
 description = (  # pylint: disable=invalid-name
     u"Linter/formatter/test-tool "
     u"wrapper for python modules")
