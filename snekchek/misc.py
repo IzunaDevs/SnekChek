3d1a5bb3fdbc6cd54a5e1f9f5bf077b265e62271
Martmists <martmists@gmail.com>
2019-10-31 15:56:21 +0100

    Update pypi tags

--- snekchek/misc.py
+++ snekchek/misc.py
@@ -1,4 +1,4 @@
 u"""Misc information for setup.py"""
 
-__version__ = u"0.4.0"
-description = u"Lint wrapper for python modules"  # pylint: disable=invalid-name
+__version__ = u"0.4.1"
+description = u"Linter/formatter/test-tool wrapper for python modules"  # pylint: disable=invalid-name
